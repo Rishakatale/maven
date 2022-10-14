5e4082943948f4847579cfab4b8f49d8e2351aec
Rish <rishkatale@gmail.com>
2022-10-14 12:19:46 +0530

    first commit

---
+++ src/main/java/main/ClassA.java
@@ -0,0 +1,20 @@
+package main;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "risha";
+		System.out.println(name);
+		
+		if(name != "risha") {
+			System.out.println("name is incorrect");
+		}else {
+			System.out.println("name is correct");
+		}
+		
+		
+	}
+
+}
